7982887431d7b2450ad4ff7db7c6c21e0936f4e4
Dan Andrei <andreidani96@gmail.com>
2016-11-28 19:50:04 +0200

    Keypress functionality.

--- index.js
+++ index.js
@@ -9,6 +9,38 @@
  *
  */
 exports.init = (scope, inst, args, data, next) => {
-    console.log(scope, inst, args);
+
+    let events = args.events || {};
+    inst.handlers = {};
+
+    // assing an event name for each of the key codes configured
+    Object.keys(events).forEach(eventName => {
+        events[eventName].forEach(handler => {
+
+            if (!handler.keyCode) return;
+
+            inst.handlers[handler.keyCode] = {
+                eventName: eventName,
+                ctrl: handler.ctrl || false,
+                shift: handler.shift || false
+            };
+        });
+    });
+
+    // listen for key presses
+    window.addEventListener('keydown', e => {
+        let handler = inst.handlers[e.keyCode];
+
+        if (handler) {
+
+            // check for modifiers
+            if (handler.ctrl !== e.ctrlKey || handler.shift !== e.shiftKey) {
+                return;
+            }
+
+            scope.flow(inst._name + '/' + handler.eventName).write({});
+        }
+    });
+
     next(null, data);
 };
