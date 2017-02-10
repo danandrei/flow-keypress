bfdf7887e5ca3e7cd6709fb1626960fb0ca3d11b
Dan Andrei <andreidani96@gmail.com>
2017-02-10 17:26:31 +0200

    Added context.

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ exports.init = (scope, state, args, data, next) => {
     let events = args.events || {};
     state.handlers = {};
 
+    // default keypress context
+    state.context = 'graph';
+
     // assing an event name for each of the key codes configured
     Object.keys(events).forEach(eventName => {
         events[eventName].forEach(handler => {
 
             if (!handler.keyCode) return;
 
-            state.handlers[handler.keyCode] = {
+            let context = handler.context || '_no_context_';
+            state.handlers[context] = state.handlers[context] || {};
+
+            state.handlers[context][handler.keyCode] = {
                 eventName: eventName,
                 ctrl: handler.ctrl || false,
                 shift: handler.shift || false
@@ -32,9 +38,14 @@ exports.init = (scope, state, args, data, next) => {
 
     // listen for key presses
     window.addEventListener('keydown', e => {
-        let handler = state.handlers[e.keyCode];
 
-        if (handler) {
+        if (!state.handlers[state.context]) { // context must exist
+            return;
+        }
+
+        let handler = state.handlers[state.context][e.keyCode];
+
+        if (handler) { // handler must exist
 
             // check for modifiers
             if (handler.ctrl !== e.ctrlKey || handler.shift !== e.shiftKey) {
@@ -44,6 +55,30 @@ exports.init = (scope, state, args, data, next) => {
             scope.flow(handler.eventName).write({ event: e });
         }
     });
+
+    next(null, data);
+};
+
+/*
+ *
+ * changes state context
+ * @name set
+ * @param {String} args.context
+ * @param {String} data.context
+ */
+exports.setContext = (scope, state, args, data, next) => {
+
+    let context = args.context || data.context;
+
+    if (!context) {
+        return next(null, data);
+    }
+
+    if (!state.handlers[context]) {
+        return next(new Error('Flow-keypress.setContext: Context does not exist'));
+    }
+
+    state.context = context;
 
     next(null, data);
 };
